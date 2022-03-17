db52da34cbfc13b1f1fac33cebe59ae618469247
Claire D. McWhite <cmcwhite@della8.princeton.edu>
2022-03-17 15:20:20 -0400

    Update breaker to py3

--- scripts/proteome_utils/proteome_breaker.py
+++ scripts/proteome_utils/proteome_breaker.py
@@ -3,37 +3,36 @@ from Bio import SeqIO
 #CDM I didn't write this, not sure where it was from
 
 def proteome_breaker(fasta_file, numseq, output_dir):
-        print "Breaking proteome into {0} sequences".format(str(numseq))
-	#read in proteome
-	proteome = SeqIO.parse(fasta_file, "fasta")
-	#get the name of the proteome
-	pid = fasta_file.replace(".fasta", "")
-	pid = pid.replace(".fa", "")
-
-	pid = pid.split('/')
-	pid = pid[len(pid)-1] 
+        print("Breaking proteome into {0} sequences".format(str(numseq)))
+        #read in proteome
+        proteome = SeqIO.parse(fasta_file, "fasta")
+        #get the name of the proteome
+        pid = fasta_file.replace(".fasta", "")
+        pid = pid.replace(".fa", "")
+        pid = pid.split('/')
+        pid = pid[len(pid)-1] 
         print(pid)
-	
-	#build up batches
-	n = 0
-	entry = True
-	while entry:
-                print n
-		batch = []	
-		while len(batch) < int(numseq):
-			try:
-				entry = proteome.next()
-			except StopIteration:
-				entry = None
-			if entry is None:
-				#End of file
-				break
-			batch.append(entry)
+        
+        #build up batches
+        n = 0
+        entry = True
+        while entry:
+                print(n)
+        batch = []        
+        while len(batch) < int(numseq):
+                try:
+                    entry = proteome.next()
+                except StopIteration:
+                    entry = None
+                if entry is None:
+                #End of file
+                    break
+                batch.append(entry)
                 print(batch)
-		if batch:
-			#SeqIO.write(batch, sys.argv[3]+pid+".scan"+str(n+1)+".fasta", "fasta")
-			SeqIO.write(batch, output_dir + pid+".seg"+str(n+1)+".fasta", "fasta")
-	        n+=1	
+        if batch:
+                #SeqIO.write(batch, sys.argv[3]+pid+".scan"+str(n+1)+".fasta", "fasta")
+                SeqIO.write(batch, output_dir + pid+".seg"+str(n+1)+".fasta", "fasta")
+                n+=1	
 
 
 parser = argparse.ArgumentParser(description='Returns a group of fasta files with specified number of sequences each (except for the last file which will likely be short)')
@@ -44,5 +43,5 @@ parser.add_argument('output_dir', action="store", type=str, help = "a directory 
 
 args = parser.parse_args()
 
-	
+        
 proteome_breaker(args.fasta_file, args.numseq, args.output_dir)
